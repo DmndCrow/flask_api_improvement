03d64484ab1b619f8648ef7bf0e25f425632d557
Aibek <aibek.ziashev@nu.edu.kz>
2021-11-01 19:05:27 +0600

    Setup basic routing in different file

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from api import create_app
+from dotenv import load_dotenv
+
+load_dotenv()
+
+port = os.getenv('FLASK_PORT')
+app = create_app()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(host='0.0.0.0', port=port, debug=True)
